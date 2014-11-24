d861c8e1e59c4eb33a24d415f8c801fae3401b83
Brian Runnells <brian.runnells@markit.com>
2014-11-24 12:05:42 -0700

    Put back movie model
    Not sure where it went

---
+++ app/models/movie.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  synopsis: DS.attr('string'),
+  critics_score: DS.attr('number'),
+  audience_score: DS.attr('number'),
+  thumbnail: DS.attr('string'),
+
+  actors: DS.hasMany('actor')
+});
