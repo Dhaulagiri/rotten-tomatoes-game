d7197324bfdfa36421532f656da63098bfdb15b5
Brian Runnells <brian.runnells@markit.com>
2014-11-24 11:54:36 -0700

    Fix bootswatch

--- Brocfile.js
+++ Brocfile.js
@@ -18,6 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-app.import('bower_components/bootswatch/lumen/bootstrap.css');
+app.import('bower_components/bootswatch/superhero/bootstrap.css');
 
 module.exports = app.toTree();
