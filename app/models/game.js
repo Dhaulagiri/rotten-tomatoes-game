c2cb07f6f434870fa84415ec5bc24ec34e75c501
Brian Runnells <brian.runnells@markit.com>
2014-11-24 17:09:53 -0700

    Building out games

--- app/models/game.js
+++ app/models/game.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
 
-  players: DS.hasMany('player')
+  movies: DS.hasMany('movie', { async: true }),
+  players: DS.hasMany('player', { async: true })
 });
--- app/models/player.js
+++ app/models/player.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
 
-  games: DS.hasMany('game')
+  games: DS.hasMany('game', { async: true })
 });
